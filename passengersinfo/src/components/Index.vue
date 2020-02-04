3e72f9950d78bf8b1a8b05893db09c958dc80725
zpx <1146735824@qq.com>
2020-02-04 19:42:58 +0800

    update

--- passengersinfo/src/components/Index.vue
+++ passengersinfo/src/components/Index.vue
@@ -1,27 +1,23 @@
 <template>
-	<div class="">
-		<el-form ref="form" :model="form" label-width="80px">
-			<el-form-item label="活动名称">
-				<el-input v-model="form.name"></el-input>
-			</el-form-item>
-		</el-form>
-	</div>
+  <div class="writePage">
+    
+  </div>
 </template>
 
 <script>
 export default {
-	name: "Index",
-	data(){
-		return {
-			form:{
-				name: 'test'
+  name: "Index",
+  data(){
+    return {
+      form:{
+        
 			}
-		}
-	}
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	
+
 </style>
--- passengersinfo/src/router/index.js
+++ passengersinfo/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Index from "@/components/Index";
-import Write from "@/components/Write";
-import Submit from "@/components/Submit";
 
 Vue.use(Router);
 
@@ -12,16 +10,6 @@ export default new Router({
       path: "/",
       name: "Index",
       component: Index
-    },
-    {
-      path: "/write",
-      name: "Write",
-      component: Write
-    },
-    {
-      path: "/submit",
-      name: "Submit",
-      component: Submit
     }
   ]
 });
